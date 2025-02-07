5079f2cad0324964fa0f16bda74afa6e949615ff
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-07 17:41:02 +0100

    new modules added and implemented into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
+/** @notice Below we can see decorator -> it adds more properties to class (metadata) */
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
