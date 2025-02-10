9679df70a0ba0d43c858cd2c6aa90576c117e4f8
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-10 20:18:56 +0100

    validators and global pipes implemented for app credentials

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config({ path: '.env.local' })
@@ -12,6 +13,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   const PORT = process.env.PORT_BACKEND || 3001
+
+  /** @dev Allows us to use validation pipes in our app */
+  /** @dev 'whitelist: true' gets rid of undefined elements in body and clears them */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
   await app.listen(PORT)
   console.log(`Server is running at port: ${PORT}`)
