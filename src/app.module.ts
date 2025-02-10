ebed983b412cf90b7463f52afe48c9cf3464d4b2
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-10 14:25:30 +0100

    connection to database implemented and prisma module added in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 /** @notice Below we can see decorator -> it adds more properties to class (metadata) */
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: '.env.local' })
+} else {
+  dotenv.config({ path: '.env.production' })
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const PORT = process.env.PORT_BACKEND || 3001
+
+  await app.listen(PORT)
+  console.log(`Server is running at port: ${PORT}`)
 }
-bootstrap();
+
+bootstrap()
