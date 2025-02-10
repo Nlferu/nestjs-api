cc9b20a4827789c2c697a5e87a1214d0a9cc8e14
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-10 20:17:27 +0100

    prisma changed to global and dotenv replaced with nestJS config

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { PrismaService } from './prisma.service'
 
+/** @dev @Global decorator makes Prisma module available for all other modules */
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
