f8d2df1d61d7f9c1edd37dbcc48a39a20946b96b
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-10 20:19:47 +0100

    signup and signin business logic implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,60 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { User, Bookmark } from '@prisma/client'
+import { AuthDto } from './dto'
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 /** @notice It provides dependency injection */
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I have signed up' }
+  async signup(dto: AuthDto) {
+    // Generate the password hash
+    const hash = await argon.hash(dto.password)
+
+    try {
+      // Save the user in the db
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash: hash },
+      })
+
+      // Do not print password in response
+      const { hash: _, ...userWithoutHash } = user
+
+      // Return the saved user
+      return { msg: 'User has been created!', userWithoutHash }
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // Duplicate field prisma code
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken')
+        }
+      }
+
+      throw error
+    }
   }
 
-  signin() {
-    return { msg: 'I have signed in' }
+  async signin(dto: AuthDto) {
+    // Find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    })
+
+    // If user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect')
+
+    // Compare passwords
+    const pwMatches = await argon.verify(user.hash, dto.password)
+
+    // If password incorrect -> throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect')
+
+    // If password correct -> send back the user
+    // Do not print password in response
+    const { hash: _, ...userWithoutHash } = user
+
+    return { msg: 'You have logged in!', userWithoutHash }
   }
 }
