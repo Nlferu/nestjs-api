2cbbbc7d955c6ad95f64429e1cb0e88a7812609e
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-14 14:13:11 +0100

    create fn updated, edit fn finished, delete fn implemented

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma/prisma.service'
 import { CreateBookmarkDto, EditBookmarkDto } from './dto'
 
@@ -6,9 +6,12 @@ import { CreateBookmarkDto, EditBookmarkDto } from './dto'
 export class BookmarkService {
   constructor(private prisma: PrismaService) {}
 
-  // create
   async createBookmark(userId: number, dto: CreateBookmarkDto) {
-    return this.prisma.bookmark.create({ data: { userId, ...dto } })
+    const bookmark = await this.prisma.bookmark.create({
+      data: { userId, ...dto },
+    })
+
+    return bookmark
   }
 
   getBookmarks(userId: number) {
@@ -33,7 +36,19 @@ export class BookmarkService {
     bookmarkId: number,
     dto: EditBookmarkDto,
   ) {
-    const bookmark = await this.prisma.user.update({
+    // Get the bookmark by id
+    const bookmark = await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    })
+
+    // Check if user owns the bookmark
+    if (!bookmark || bookmark.userId !== userId)
+      throw new ForbiddenException('Access to resource denied')
+
+    // Edit bookmark
+    return this.prisma.bookmark.update({
       where: {
         id: bookmarkId,
       },
@@ -41,9 +56,21 @@ export class BookmarkService {
         ...dto,
       },
     })
-
-    return bookmark
   }
 
-  async deleteBookmarkById(userId: number, bookmarkId: number) {}
+  async deleteBookmarkById(userId: number, bookmarkId: number) {
+    // Get the bookmark by id
+    const bookmark = await this.getBookmarkById(userId, bookmarkId)
+
+    // Check if user owns the bookmark
+    if (!bookmark || bookmark.userId !== userId)
+      throw new ForbiddenException('Access to resource denied')
+
+    // Delete bookmark
+    return this.prisma.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    })
+  }
 }
